c8362d7fb8f017133e5aa742474987942278b7a5
Lekhani Shukla <lekhanishukla@gmail.com>
2021-04-28 23:00:00 +0530

    updated url for static files

--- healthCareManagment/urls.py
+++ healthCareManagment/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('management.urls'))
-]+ static('/static', document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('management.urls'))
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
